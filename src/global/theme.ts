aa7953affebea82199b852469d91a88e4c36f3be
Snaek7 <emerson_franca77@hotmail.com>
2021-09-08 20:52:01 -0300

    global theme

---
+++ src/global/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
+import green from '@material-ui/core/colors/green'
+import { blue } from '@material-ui/core/colors'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[600],
+    },
+  },
+})
