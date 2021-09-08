876ed083773f10e0186e1254f55ce21285150cd4
Snaek7 <emerson_franca77@hotmail.com>
2021-09-08 20:52:13 -0300

    Header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles.scss'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header__img"
+        src="https://rarolabs.com.br/assets/logo-cae9beb1976500005b5e29dea19bdaa2761082232703a16beb71ffd2117014e5.png"
+      />
+    </header>
+  )
+}
